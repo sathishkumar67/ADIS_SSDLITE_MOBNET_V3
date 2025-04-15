3b8c433eda6ef22f92e34398325256fbd2884a2d
sathishkumar67 <sathishvillers2000@gmail.com>
2025-04-15 10:28:01 +0530

    added modules

--- ssd_mobnetv3_adis/__init__.py
+++ ssd_mobnetv3_adis/__init__.py
@@ -1,4 +1,6 @@
 from .utils import unzip_file
 from .dataset import SSDLITEOBJDET_DATASET, CachedSSDLITEOBJDET_DATASET
+from .model import SSD_MOBILENET_V3_Large
 
-__all__ = ["unzip_file", "SSDLITEOBJDET_DATASET", "CachedSSDLITEOBJDET_DATASET"]
+# list of all modules to be imported when using 'from ssd_mobnetv3_adis import *'
+__all__ = ["unzip_file", "SSDLITEOBJDET_DATASET", "CachedSSDLITEOBJDET_DATASET", "SSD_MOBILENET_V3_Large"]
