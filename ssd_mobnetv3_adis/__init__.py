3042a3e7cd8a698399b434959c99f75c94d42881
sathishkumar67 <sathishvillers2000@gmail.com>
2025-04-15 11:28:49 +0530

    removed huggingface module

--- ssd_mobnetv3_adis/__init__.py
+++ ssd_mobnetv3_adis/__init__.py
@@ -1,7 +1,6 @@
 from .utils import unzip_file
 from .dataset import SSDLITEOBJDET_DATASET, CachedSSDLITEOBJDET_DATASET
 from .model import SSD_MOBILENET_V3_Large
-from huggingface_hub import hf_hub_download
 
 # list of all modules to be imported when using 'from ssd_mobnetv3_adis import *'
-__all__ = ["unzip_file", "SSDLITEOBJDET_DATASET", "CachedSSDLITEOBJDET_DATASET", "SSD_MOBILENET_V3_Large", "hf_hub_download"]
+__all__ = ["unzip_file", "SSDLITEOBJDET_DATASET", "CachedSSDLITEOBJDET_DATASET", "SSD_MOBILENET_V3_Large"]
