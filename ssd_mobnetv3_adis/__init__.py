b4d59fe57d1bf8b57ec286cbeb151033dda7f68a
sathishkumar67 <sathishvillers2000@gmail.com>
2025-04-15 12:09:26 +0530

    refactored a bit

--- ssd_mobnetv3_adis/__init__.py
+++ ssd_mobnetv3_adis/__init__.py
@@ -3,4 +3,9 @@ from .dataset import SSDLITEOBJDET_DATASET, CachedSSDLITEOBJDET_DATASET
 from .model import SSD_MOBILENET_V3_Large, SSD_MOBILENET_V3_Large_Lightning_Config, SSD_MOBILENET_V3_Large_Lightning
 
 # list of all modules to be imported when using 'from ssd_mobnetv3_adis import *'
-__all__ = ("unzip_file", "SSDLITEOBJDET_DATASET", "CachedSSDLITEOBJDET_DATASET", "SSD_MOBILENET_V3_Large", "SSD_MOBILENET_V3_Large_Lightning_Config","SSD_MOBILENET_V3_Large_Lightning")
+__all__ = ("unzip_file", 
+        "SSDLITEOBJDET_DATASET", 
+        "CachedSSDLITEOBJDET_DATASET", 
+        "SSD_MOBILENET_V3_Large", 
+        "SSD_MOBILENET_V3_Large_Lightning_Config",
+        "SSD_MOBILENET_V3_Large_Lightning")
