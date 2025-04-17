776aef8cb59040d5b412c0aa0dddfc319cf680a1
sathishkumar67 <sathishvillers2000@gmail.com>
2025-04-17 09:32:45 +0530

    removed config import

--- ssd_mobnetv3_adis/__init__.py
+++ ssd_mobnetv3_adis/__init__.py
@@ -1,11 +1,10 @@
 from .utils import unzip_file
 from .dataset import SSDLITEOBJDET_DATASET, CachedSSDLITEOBJDET_DATASET, collate_fn
-from .model import SSD_MOBILENET_V3_Large_Config, SSD_MOBILENET_V3_Large 
+from .model import SSD_MOBILENET_V3_Large 
 
 # list of all modules to be imported when using 'from ssd_mobnetv3_adis import *'
 __all__ = ("unzip_file", 
         "collate_fn",
         "SSDLITEOBJDET_DATASET", 
         "CachedSSDLITEOBJDET_DATASET", 
-        "SSD_MOBILENET_V3_Large_Config",
         "SSD_MOBILENET_V3_Large")
