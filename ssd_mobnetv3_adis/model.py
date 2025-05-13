695e65c970f137e188305e7b06c596e13797fd73
sathishkumar67 <sathishvillers2000@gmail.com>
2025-05-13 20:26:53 +0530

    refactored a bit

--- ssd_mobnetv3_adis/model.py
+++ ssd_mobnetv3_adis/model.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 from typing import Tuple
-import time
-from tqdm import tqdm
 from functools import partial
 import torch
 import torch.nn as nn
@@ -9,7 +7,6 @@ import torch.optim as optim
 from torchvision.models.detection import ssdlite320_mobilenet_v3_large
 from torchvision.models.detection.ssdlite import SSDLiteClassificationHead
 from torchvision.models.detection import _utils as det_utils
-from torchmetrics.detection import MeanAveragePrecision 
 
 
 class SSDLITE_MOBILENET_V3_Large(nn.Module):
@@ -31,8 +28,8 @@ class SSDLITE_MOBILENET_V3_Large(nn.Module):
         self.img_size = img_size
         
         # initialize the model
-        # model weights can be changed based on the requirements
         self.model = ssdlite320_mobilenet_v3_large(weights='COCO_V1', weights_backbone="DEFAULT")
+        
         # modify the model to use the specified number of classes
         self.model.head.classification_head = SSDLiteClassificationHead(
             in_channels=det_utils.retrieve_out_channels(self.model.backbone, (self.img_size, self.img_size)),
@@ -40,7 +37,8 @@ class SSDLITE_MOBILENET_V3_Large(nn.Module):
             num_classes=self.num_classes_with_bg,
             norm_layer=partial(nn.BatchNorm2d, eps=0.001, momentum=0.03)
         )
-        self.model.detections_per_img = 50 
+        # set the nominal number of detections per image
+        self.model.detections_per_img = 100 
 
     
     def configure_optimizers(self, lr: float = 0.0001, betas: Tuple[float, float] = (0.9, 0.999), weight_decay: float = 0.0001, eps: float = 1e-08, fused: bool = True) -> torch.optim.Optimizer:  
@@ -74,8 +72,7 @@ class SSDLITE_MOBILENET_V3_Large(nn.Module):
                                     eps=eps, 
                                     fused=fused)
     
-    
-    def forward(self, images: torch.Tensor, targets: dict=None) :
+    def forward(self, images: torch.Tensor, targets: dict=None):
         """
         Forward pass through the model.
 
@@ -87,66 +84,3 @@ class SSDLITE_MOBILENET_V3_Large(nn.Module):
             torch.Tensor: Model output predictions.
         """
         return self.model(images, targets)
-    
-    
-    def load(self, checkpoint_path: str, key_name: str = "model_state_dict", map_location: str = "cpu") -> None:
-        """
-        Load the model state dict from a checkpoint file.
-
-        Args:
-            checkpoint_path (str): Path to the checkpoint file.
-            key_name (str): Key name in the checkpoint file to load the model state dict.
-            map_location (str): Map location for loading the checkpoint.
-        """
-        start_time = time.time()
-        print(f"Loading checkpoint from {checkpoint_path}...")
-        self.load_state_dict(torch.load(checkpoint_path, map_location=map_location)[key_name])
-        print(f"Checkpoint loaded in {time.time() - start_time:.2f} seconds.")
-        
-        
-    def evaluate(self, dataloaders: dict[str, torch.utils.data.DataLoader], device: torch.device|str) -> dict[str, dict[str, float]]:
-        """
-        Evaluate the model on the given dataloaders.
-        
-        Args:
-            dataloaders (dict[str, torch.utils.data.DataLoader]): A dictionary containing the dataloaders for each split (train, val, test).
-            device (torch.device|str): The device to run the evaluation on.
-        """
-        
-        # initialize the metric
-        metric = MeanAveragePrecision(
-        iou_type="bbox",
-        class_metrics=True,
-        extended_summary=True)
-        
-        # get the dataloaders
-        splits = dataloaders.keys()
-        loaders = [dataloaders[split] for split in splits]
-        
-        # if device is a string
-        if isinstance(device, str):
-            device = device
-        else:
-            device = f"{device.type}:{device.index}"
-        
-        # run the evaluation
-        print("Starting evaluation...")
-        results = {}
-        for split, loader in zip(splits, loaders):
-            print(f"Evaluating {split} set")
-            # set the model to evaluation mode
-            self.eval()
-            # reset the metric 
-            metric.reset()
-            progress_bar = tqdm(loader, desc=f"Evaluating {split} set", unit="batch")
-            with torch.no_grad():
-                for images, targets in progress_bar:
-                    # pass the images only to the model
-                    outputs = self(images)
-                    # update the metric with the outputs and targets
-                    metric.update(outputs, targets)
-                # compute the metric, store the results
-                results[split] =  metric.compute()
-                
-        print("Evaluation complete.")
-        return results
